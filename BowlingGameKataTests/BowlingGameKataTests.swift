071b1eff97b3cce69db9dc8e65ccd73b4b97f705
Bilal Bas <bilal.bas@gmx.de>
2025-04-05 14:44:42 +0200

    roll without bonus

--- BowlingGameKataTests/BowlingGameKataTests.swift
+++ BowlingGameKataTests/BowlingGameKataTests.swift
@@ -1,11 +1,14 @@
 import Testing
 
 public class BowlingGame {
+    private var _score = 0
+
     public func roll(_ pins: Int) {
+        _score += pins
     }
     
     public func score() -> Int {
-        0
+        _score
     }
 }
 
@@ -20,6 +23,17 @@ struct BowlingGameKataTests {
         }
         
         #expect(sut.score() == 0)
+    }
+    
+    @Test
+    func roll_withoutBonus() {
+        let sut = BowlingGame()
+        
+        sut.roll(5)
+        sut.roll(3)
+        
+        #expect(sut.score() == 8)
+
     }
 
 }
