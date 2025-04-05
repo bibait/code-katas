539667d07228f01902b5f4849a3322aa70fa0db9
Bilal Bas <bilal.bas@gmx.de>
2025-04-05 14:42:34 +0200

    gutter game

--- BowlingGameKataTests/BowlingGameKataTests.swift
+++ BowlingGameKataTests/BowlingGameKataTests.swift
@@ -1,14 +1,25 @@
 import Testing
 
 public class BowlingGame {
+    public func roll(_ pins: Int) {
+    }
     
+    public func score() -> Int {
+        0
+    }
 }
 
 struct BowlingGameKataTests {
 
     @Test
-    func canInit() {
+    func gutterGame() {
         let sut = BowlingGame()
+        
+        for _ in 0..<20 {
+            sut.roll(0)
+        }
+        
+        #expect(sut.score() == 0)
     }
 
 }
