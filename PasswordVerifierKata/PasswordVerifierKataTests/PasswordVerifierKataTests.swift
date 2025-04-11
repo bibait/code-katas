b47f58439a1395309f9a36bcc1883dfc4a4211b2
Bilal Bas <bilal.bas@gmx.de>
2025-04-11 21:12:20 +0200

    mark as checked

--- PasswordVerifierKata/PasswordVerifierKataTests/PasswordVerifierKataTests.swift
+++ PasswordVerifierKata/PasswordVerifierKataTests/PasswordVerifierKataTests.swift
@@ -116,7 +116,7 @@ public class PasswordRuleFactory {
  2. password should not be null ✅
  3. password should have one uppercase letter at least ✅
  4. password should have one lowercase letter at least ✅
- 5. password should have one number at least
+ 5. password should have one number at least ✅
  
  Each one of these should throw an exception with a different message of your choosing.
  
