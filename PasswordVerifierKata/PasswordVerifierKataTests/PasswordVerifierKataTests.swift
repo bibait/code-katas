eb6a31af6f3740ef2c83ba97f1e51940d06ab8d6
Bilal Bas <bilal.bas@gmx.de>
2025-04-11 20:47:45 +0200

    throws error

--- PasswordVerifierKata/PasswordVerifierKataTests/PasswordVerifierKataTests.swift
+++ PasswordVerifierKata/PasswordVerifierKataTests/PasswordVerifierKataTests.swift
@@ -3,6 +3,10 @@ import Testing
 
 public class PasswordVerifier {
     public init() {}
+    
+    public func verify(_ password: String) throws {
+        throw NSError(domain: "Test", code: -1)
+    }
 }
 
 /**
@@ -14,6 +18,8 @@ public class PasswordVerifier {
  4. password should have one lowercase letter at least
  5. password should have one number at least
  
+ Each one of these should throw an exception with a different message of your choosing.
+ 
  6. Add feature: password is OK if at least three of the previous conditions is true
  7. Add feature: password is never OK if item 4 is not true
  */
@@ -21,8 +27,12 @@ public class PasswordVerifier {
 struct PasswordVerifierKataTests {
 
     @Test
-    func canInit() {
+    func lessThanEightCharacters_throwsError() {
         let sut = PasswordVerifier()
+        
+        #expect(throws: (any Error).self) {
+            try sut.verify("invalid")
+        }
     }
 
 }
