8dd3146f6c5f5372042207a3d2dc180c203a121f
Bilal Bas <bilal.bas@gmx.de>
2025-04-11 20:39:58 +0200

    Create PasswordVerifier kata

---
+++ PasswordVerifierKata/PasswordVerifierKata/PasswordVerifierKata.h
@@ -0,0 +1,18 @@
+//
+//  PasswordVerifierKata.h
+//  PasswordVerifierKata
+//
+//  Created by Bilal Baş on 11.04.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordVerifierKata.
+FOUNDATION_EXPORT double PasswordVerifierKataVersionNumber;
+
+//! Project version string for PasswordVerifierKata.
+FOUNDATION_EXPORT const unsigned char PasswordVerifierKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordVerifierKata/PublicHeader.h>
+
+
---
+++ PasswordVerifierKata/PasswordVerifierKataTests/PasswordVerifierKataTests.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordVerifierKataTests.swift
+//  PasswordVerifierKataTests
+//
+//  Created by Bilal Baş on 11.04.25.
+//
+
+import Testing
+@testable import PasswordVerifierKata
+
+struct PasswordVerifierKataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
