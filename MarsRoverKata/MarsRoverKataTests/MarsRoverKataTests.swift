c6ecf1b85794a6564f238e6d54a2fdafb07ab82e
Bilal Bas <bilal.bas@gmx.de>
2025-04-20 16:30:48 +0200

    can init

--- MarsRoverKata/MarsRoverKataTests/MarsRoverKataTests.swift
+++ MarsRoverKata/MarsRoverKataTests/MarsRoverKataTests.swift
@@ -1,11 +1,80 @@
 import Testing
 import MarsRoverKata
 
+public struct Position {
+    let position: Coordinate
+    let direction: Direction
+    
+    public init(position: Coordinate, direction: Direction) {
+        self.position = position
+        self.direction = direction
+    }
+}
+
+public enum Direction {
+    case north, south, east, west
+}
+
+public struct Coordinate {
+    let x: Int
+    let y: Int
+    
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+public struct Map {
+    let tiles: [[Tile]]
+    
+    public init(tiles: [[Tile]]) {
+        self.tiles = tiles
+    }
+}
+
+public struct Tile {
+    let isObstacle: Bool
+    
+    public init(isObstacle: Bool) {
+        self.isObstacle = isObstacle
+    }
+}
+
+public class MarsRover {
+    private let startingPosition: Position
+    private let map: Map
+    
+    public init(startingPosition: Position, map: Map) {
+        self.startingPosition = startingPosition
+        self.map = map
+    }
+}
+
 struct MarsRoverKataTests {
 
     @Test
-    func zero() {
-        
+    func canInit() {
+        let _ = MarsRover(
+            startingPosition: .init(
+                position: Coordinate(x: 0, y: 0),
+                direction: .east
+            ),
+            map: .init(tiles: [
+                [
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                ],
+                [
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                    .init(isObstacle: false),
+                ],
+            ])
+        )
     }
 
 }
