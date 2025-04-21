fe937d81e14ff1635736e4637665c78dee5a3a1d
Bilal Bas <bilal.bas@gmx.de>
2025-04-21 16:33:57 +0200

    move FakeRepository to its own file

---
+++ TodoAppKata/TodoAppKataTests/FakeRepository.swift
@@ -0,0 +1,35 @@
+import TodoAppKata
+
+class FakeRepository: TodoItemRepository {
+    private(set) var savedTodos: [TodoItem] = []
+    private(set) var removedTodos: [TodoItem] = []
+    private(set) var error: Error?
+
+    func save(_ todo: TodoItem) throws {
+        if let error = error {
+            throw error
+        }
+
+        savedTodos.append(todo)
+    }
+    
+    func remove(_ todo: TodoItem) throws {
+        if let error = error {
+            throw error
+        }
+
+        removedTodos.append(todo)
+    }
+    
+    func fetchAllItems() -> [TodoItem] {
+        savedTodos
+    }
+    
+    func stub(items: [TodoItem]) {
+        savedTodos = items
+    }
+    
+    func stub(error: Error) {
+        self.error = error
+    }
+}
