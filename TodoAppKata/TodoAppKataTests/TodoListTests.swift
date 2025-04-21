d21aefb72b5cf47f3f91eacdf88ee2aa2820f761
Bilal Bas <bilal.bas@gmx.de>
2025-04-21 15:48:58 +0200

    add todo

--- TodoAppKata/TodoAppKataTests/TodoListTests.swift
+++ TodoAppKata/TodoAppKataTests/TodoListTests.swift
@@ -2,14 +2,37 @@ import Testing
 import TodoAppKata
 
 public class TodoList {
+    private var _todos: [TodoItem] = []
+
     public init() {}
+    
+    public var todos: [TodoItem] {
+        _todos
+    }
+    
+    public func add(_ todo: TodoItem) {
+        _todos.append(todo)
+    }
+}
+
+public struct TodoItem: Equatable {
+    public let title: String
+    
+    public init(title: String) {
+        self.title = title
+    }
 }
 
 struct TodoListTests {
 
     @Test
-    func canInit() {
-        let _ = TodoList()
+    func addTodo() {
+        let sut = TodoList()
+        let newTodo = TodoItem(title: "New Todo")
+        
+        sut.add(newTodo)
+        
+        #expect(sut.todos == [newTodo])
     }
 
 }
