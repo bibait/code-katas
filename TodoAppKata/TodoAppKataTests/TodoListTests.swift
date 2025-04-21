5b91ac47351ca861d77d1fa52d3a803f8c7ba15c
Bilal Bas <bilal.bas@gmx.de>
2025-04-21 15:45:46 +0200

    can init

---
+++ TodoAppKata/TodoAppKataTests/TodoListTests.swift
@@ -0,0 +1,15 @@
+import Testing
+import TodoAppKata
+
+public class TodoList {
+    public init() {}
+}
+
+struct TodoListTests {
+
+    @Test
+    func canInit() {
+        let _ = TodoList()
+    }
+
+}
