bc357cc33644ebf52a9d87372ed3997987728349
Bilal Bas <bilal.bas@gmx.de>
2025-04-26 11:42:26 +0200

    extend test cases (with duplicates and negatives)

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
@@ -53,6 +53,9 @@ struct SelectionSortTests {
     
     @Test(arguments: [
         ([2, 1], [1, 2]),
+        ([2, 1, 3, 10, 20], [1, 2, 3, 10, 20]),
+        ([2, 1, -1, -10, 123], [-10, -1, 1, 2, 123]),
+        ([2, 1, 1, 10, 1, 2, 10, 30], [1, 1, 1, 2, 2, 10, 10, 30]),
     ])
     func sort_withUnsortedArray_shouldReturnSorted(values: [Int], expected: [Int]) {
         #expect(sort(values: values) == expected)
