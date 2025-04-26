4c5c0a31297fdeab048ae2c3c180f63733c0ce70
Bilal Bas <bilal.bas@gmx.de>
2025-04-26 11:43:59 +0200

    make generic

--- GrokkingAlgorithms/GrokkingAlgorithms/SelectionSort.swift
+++ GrokkingAlgorithms/GrokkingAlgorithms/SelectionSort.swift
@@ -1,12 +1,12 @@
 public class SelectionSort {
-    public static func sort(values: [Int]) -> [Int] {
+    public static func sort<T: Comparable>(values: [T]) -> [T] {
         if values.count <= 1 {
             return values
         }
         
         var values = values
         let count = values.count
-        var result = [Int]()
+        var result = [T]()
         
         while result.count != count {
             let (min, index) = minValue(in: values)
@@ -18,8 +18,8 @@ public class SelectionSort {
         return result
     }
     
-    private static func minValue(in values: [Int]) -> (Int, Int) {
-        var result: (Int, Int)?
+    private static func minValue<T: Comparable>(in values: [T]) -> (T, Int) {
+        var result: (T, Int)?
 
         for (index, value) in values.enumerated() {
             if result == nil {
