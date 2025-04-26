7e8d55b1cf8306c12a6f828dc07e1b785d0c07e2
Bilal Bas <bilal.bas@gmx.de>
2025-04-26 12:39:10 +0200

    simplify test suite

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
@@ -3,41 +3,30 @@ import GrokkingAlgorithms
 
 struct SelectionSortTests {
     
-    @Test
-    func sort_withNoElements_shouldReturnEmptyArray() {
-        #expect(sortDescending(values: []).isEmpty)
-    }
-    
-    @Test
-    func sort_withOneElement_shouldReturnArray() {
-        #expect(sortDescending(values: [1]) == [1])
-    }
-    
     @Test(arguments: [
+        ([], []),
+        ([1], [1]),
         ([2, 1], [1, 2]),
         ([1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]),
         ([2, 1, 3, 10, 20], [1, 2, 3, 10, 20]),
         ([2, 1, -1, -10, 123], [-10, -1, 1, 2, 123]),
         ([2, 1, 1, 10, 1, 2, 10, 30], [1, 1, 1, 2, 2, 10, 10, 30]),
     ])
-    func sort_withUnsortedArray_shouldReturnAscending(values: [Int], expected: [Int]) {
+    func sortAscending(values: [Int], expected: [Int]) {
         #expect(sortAscending(values: values) == expected)
     }
     
     @Test(arguments: [
+        ([], []),
+        ([1], [1]),
         ([2, 1], [2, 1]),
         ([1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]),
         ([2, 1, 3, 10, 20], [20, 10, 3, 2, 1]),
         ([2, 1, -1, -10, 123], [123, 2, 1, -1, -10]),
         ([2, 1, 1, 10, 1, 2, 10, 30], [30, 10, 10, 2, 2, 1, 1, 1]),
     ])
-    func sort_withUnsortedArray_shouldReturnDescending(values: [Int], expected: [Int]) {
+    func sortDescending(values: [Int], expected: [Int]) {
         #expect(sortDescending(values: values) == expected)
-    }
-    
-    @Test
-    func sort_withSortedArray_shouldReturnArray() {
-        #expect(sortAscending(values: [1, 2, 30, 1234, 12341234]) == [1, 2, 30, 1234, 12341234])
     }
     
     // MARK: - Helpers
