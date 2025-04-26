7ce5e4e9e404634bb7b76d9c8873753e46c0fde3
Bilal Bas <bilal.bas@gmx.de>
2025-04-26 15:41:24 +0200

    Set up project

---
+++ TripServiceKata/TripServiceKataTests/TripServiceKataTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripServiceKataTests.swift
+//  TripServiceKataTests
+//
+//  Created by Bilal Baş on 26.04.25.
+//
+
+import Testing
+@testable import TripServiceKata
+
+struct TripServiceKataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
