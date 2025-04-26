d902db9d835be430eee194ca786790824bf37cce
Bilal Bas <bilal.bas@gmx.de>
2025-04-26 11:39:22 +0200

    sort

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/SelectionSortTests.swift
@@ -3,7 +3,39 @@ import GrokkingAlgorithms
 
 public class SelectionSort {
     static func sort(values: [Int]) -> [Int] {
-        values
+        if values.count <= 1 {
+            return values
+        }
+        
+        var values = values
+        let count = values.count
+        var result = [Int]()
+        
+        while result.count != count {
+            let (min, index) = minValue(in: values)
+            
+            result.append(min)
+            values.remove(at: index)
+        }
+        
+        return result
+    }
+    
+    private static func minValue(in values: [Int]) -> (Int, Int) {
+        var result: (Int, Int)?
+
+        for (index, value) in values.enumerated() {
+            if result == nil {
+                result = (value, index)
+                continue
+            }
+            
+            if value < result!.0 {
+                result = (value, index)
+            }
+        }
+        
+        return result!
     }
 }
 
@@ -17,6 +49,11 @@ struct SelectionSortTests {
     @Test
     func sort_withOneElement_shouldReturnArray() {
         #expect(sort(values: [1]) == [1])
+    }
+    
+    @Test
+    func sort_witTwoElements_shouldReturnSorted() {
+        #expect(sort(values: [2, 1]) == [1, 2])
     }
     
     // MARK: - Helpers
