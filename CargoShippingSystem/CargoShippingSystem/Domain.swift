2d2fbd9a23669775464b81f3f5a07f1d66e63860
Bilal Bas <bilal.bas@gmx.de>
2025-04-27 17:38:02 +0200

    set up project with domain objects

---
+++ CargoShippingSystem/CargoShippingSystem/Domain.swift
@@ -0,0 +1,27 @@
+struct Customer {
+    let customerId: String
+    let name: String
+}
+
+struct Cargo {
+    let trackingId: String
+}
+
+struct DeliveryHistory {}
+
+struct DeliverySpecification {
+    let arrivalTime: Date
+}
+
+struct HandlingEvent {
+    let completionTime: Date
+    let type: String
+}
+
+struct CarrierMovement {
+    let scheduleId: String
+}
+
+struct Location {
+    let portCode: String
+}
---
+++ CargoShippingSystem/CargoShippingSystemTests/CargoShippingSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  CargoShippingSystemTests.swift
+//  CargoShippingSystemTests
+//
+//  Created by Bilal Baş on 27.04.25.
+//
+
+import Testing
+@testable import CargoShippingSystem
+
+struct CargoShippingSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
