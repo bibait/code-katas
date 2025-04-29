830537e5cdfd3e39552d254ea53cb13a89850155
Bilal Bas <bilal.bas@gmx.de>
2025-04-29 19:26:04 +0200

    create Factorial class

--- GrokkingAlgorithms/GrokkingAlgorithms/Factorial.swift
+++ GrokkingAlgorithms/GrokkingAlgorithms/Factorial.swift
@@ -1,3 +1,9 @@
+public class Factorial {
+    public static func compute(_ number: Int) -> Int {
+        number == 1 ? 1 : number * compute(number - 1)
+    }
+}
+
 public func factorial(_ n: Int) -> Int {
     n == 1 ? 1 : n * factorial(n - 1)
 }
