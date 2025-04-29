4f7fbfebf542c487520834ba0c6d5b81aa55a935
Bilal Bas <bilal.bas@gmx.de>
2025-04-29 19:13:28 +0200

    remove redundant tests

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/RecursionTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/RecursionTests.swift
@@ -17,26 +17,5 @@ struct RecursionTests {
         
         #expect(result == expected)
     }
-
-    @Test
-    func factorial_zero_shouldReturnZero() {
-        let result = factorial(0)
-        
-        #expect(result == 0)
-    }
-    
-    @Test
-    func factorial_one_shouldReturnOne() {
-        let result = factorial(1)
-        
-        #expect(result == 1)
-    }
-    
-    @Test
-    func factorial_two_shouldReturnTwo() {
-        let result = factorial(2)
-        
-        #expect(result == 2)
-    }
     
 }
