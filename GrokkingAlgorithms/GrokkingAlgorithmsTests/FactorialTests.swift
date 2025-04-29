dd537a7ade0e78dd7187c48e69e99f47f90e8534
Bilal Bas <bilal.bas@gmx.de>
2025-04-29 19:25:21 +0200

    remove redundant tests

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/FactorialTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/FactorialTests.swift
@@ -12,26 +12,6 @@ struct FactorialTests {
         #expect(compute(5) == 120)
     }
     
-    func factorial_1() {
-        #expect(compute(1) == 1)
-    }
-    
-    func factorial_2() {
-        #expect(compute(2) == 2)
-    }
-    
-    func factorial_3() {
-        #expect(compute(3) == 6)
-    }
-    
-    func factorial_4() {
-        #expect(compute(4) == 24)
-    }
-    
-    func factorial_5() {
-        #expect(compute(5) == 120)
-    }
-    
     // MARK: - Helpers
     
     private func compute(_ number: Int) -> Int {
