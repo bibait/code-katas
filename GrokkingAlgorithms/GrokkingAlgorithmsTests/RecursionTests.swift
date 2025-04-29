c22cafb7647fac5a4303094b3541cec66ea4d674
Bilal Bas <bilal.bas@gmx.de>
2025-04-29 19:02:34 +0200

    factorial 0

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/RecursionTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/RecursionTests.swift
@@ -1,11 +1,17 @@
 import Testing
 import GrokkingAlgorithms
 
+func factorial(_ n: Int) -> Int {
+    0
+}
+
 struct RecursionTests {
     
     @Test
-    func zero() {
+    func factorial_zero_shouldReturnZero() {
+        let result = factorial(0)
         
+        #expect(result == 0)
     }
     
 }
