80c08ded7607254b04fb9565593b05755e246d15
Bilal Bas <bilal.bas@gmx.de>
2025-05-03 12:56:57 +0200

    parameterize test

--- GrokkingAlgorithms/GrokkingAlgorithmsTests/FactorialTests.swift
+++ GrokkingAlgorithms/GrokkingAlgorithmsTests/FactorialTests.swift
@@ -3,29 +3,15 @@ import GrokkingAlgorithms
 
 struct FactorialTests {
     
-    @Test
-    func factorial_1() {
-        #expect(compute(1) == 1)
-    }
-    
-    @Test
-    func factorial_2() {
-        #expect(compute(2) == 2)
-    }
-    
-    @Test
-    func factorial_3() {
-        #expect(compute(3) == 6)
-    }
-    
-    @Test
-    func factorial_4() {
-        #expect(compute(4) == 24)
-    }
-    
-    @Test
-    func factorial_5() {
-        #expect(compute(5) == 120)
+    @Test(arguments: [
+        (1, 1),
+        (2, 2),
+        (3, 6),
+        (4, 24),
+        (5, 120),
+    ])
+    func factorial(number: Int, expected: Int) {
+        #expect(compute(number) == expected)
     }
     
     // MARK: - Helpers
