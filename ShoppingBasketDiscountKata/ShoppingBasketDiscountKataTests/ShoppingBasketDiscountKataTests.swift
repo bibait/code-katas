72bdaba8eac851be98ac44f75ea0f7d5b53d67a5
Bilal Bas <bilal.bas@gmx.de>
2025-05-31 15:47:54 +0200

    getItemQuanity

--- ShoppingBasketDiscountKata/ShoppingBasketDiscountKataTests/ShoppingBasketDiscountKataTests.swift
+++ ShoppingBasketDiscountKata/ShoppingBasketDiscountKataTests/ShoppingBasketDiscountKataTests.swift
@@ -1,10 +1,40 @@
 import Testing
 
+public struct Item {
+    private let name: String
+    private let price: Float
+    
+    public init(name: String, price: Float) {
+        self.name = name
+        self.price = price
+    }
+}
+
+public class ShoppingBasket {
+    private let items: [Item]
+    
+    public init(items: [Item]) {
+        self.items = items
+    }
+    
+    public func getItemQuantity(name: String) -> Int {
+        return 2
+    }
+}
+
 struct ShoppingBasketDiscountKataTests {
 
     @Test
-    func zero() {
+    func getItemQuantity_shouldReturnCount() {
+        let sut = ShoppingBasket(items: [
+            Item(name: "Apple", price: 1.0),
+            Item(name: "Apple", price: 1.0),
+            Item(name: "Banana", price: 1.0),
+        ])
         
+        let result = sut.getItemQuantity(name: "Apple")
+        
+        #expect(result == 2)
     }
 
 }
