acdc97b097a1cec533c86121dec03e10f2c1c9d6
Bilal Bas <bilal.bas@gmx.de>
2025-06-04 17:15:39 +0200

    can be initialized max and min height

--- HeightAdjustableDesk/HeightAdjustableDeskTests/HeightAdjustableDeskTests.swift
+++ HeightAdjustableDesk/HeightAdjustableDeskTests/HeightAdjustableDeskTests.swift
@@ -1,11 +1,20 @@
 import Testing
 
 public class HeightAdjustableDesk {
-    private(set) var height: Double
+    private(set) public var height: Double
+    private(set) public var maxHeight: Double
+    private(set) public var minHeight: Double
 
-    public init(height: Double = 100) {
+    public init(
+        height: Double = 100,
+        maxHeight: Double = 150,
+        minHeight: Double = 50
+    ) {
         self.height = height
+        self.maxHeight = maxHeight
+        self.minHeight = minHeight
     }
+    
 }
 
 struct HeightAdjustableDeskTests {
@@ -15,6 +24,14 @@ struct HeightAdjustableDeskTests {
         let sut = HeightAdjustableDesk()
         
         #expect(sut.height == 100)
+    }
+    
+    @Test
+    func hasMaxAndMinHeight() {
+        let sut = HeightAdjustableDesk(maxHeight: 150, minHeight: 50)
+        
+        #expect(sut.maxHeight == 150)
+        #expect(sut.minHeight == 50)
     }
 
 }
