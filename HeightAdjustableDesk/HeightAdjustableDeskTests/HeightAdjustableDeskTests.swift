ad874777b8bd81fbd0c330f5313c95d1ab81700b
Bilal Bas <bilal.bas@gmx.de>
2025-06-04 17:17:28 +0200

    does not change position if no position saved

--- HeightAdjustableDesk/HeightAdjustableDeskTests/HeightAdjustableDeskTests.swift
+++ HeightAdjustableDesk/HeightAdjustableDeskTests/HeightAdjustableDeskTests.swift
@@ -4,7 +4,7 @@ public class HeightAdjustableDesk {
     private(set) public var height: Double
     private(set) public var maxHeight: Double
     private(set) public var minHeight: Double
-
+    
     public init(
         initialHeight: Double = 100,
         maxHeight: Double = 150,
@@ -15,6 +15,8 @@ public class HeightAdjustableDesk {
         self.minHeight = minHeight
     }
     
+    public func applySavedPosition(number: Int) {
+    }
 }
 
 struct HeightAdjustableDeskTests {
@@ -32,6 +34,23 @@ struct HeightAdjustableDeskTests {
         
         #expect(sut.maxHeight == 150)
         #expect(sut.minHeight == 50)
+    }
+    
+    @Test
+    func applySavedPosition_withoutSaving_staysAtSamePosition() {
+        let sut = HeightAdjustableDesk()
+        
+        sut.applySavedPosition(number: 1)
+        #expect(sut.height == 100)
+        
+        sut.applySavedPosition(number: 2)
+        #expect(sut.height == 100)
+        
+        sut.applySavedPosition(number: 3)
+        #expect(sut.height == 100)
+        
+        sut.applySavedPosition(number: 4)
+        #expect(sut.height == 100)
     }
 
 }
